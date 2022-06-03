459bbecdd7e087eda69c229fedbdf4235e2d84e7
Gabriela <96033603+Gabrielasl18@users.noreply.github.com>
2022-06-03 13:13:36 -0300

    Update pilha.cpp

--- aulaPraticaEDA/pilha.cpp
+++ aulaPraticaEDA/pilha.cpp
@@ -9,6 +9,12 @@ struct no{
 typedef struct no *noPtr;
 noPtr topo = NULL;
 
+bool listaVazia(){
+    if(topo == NULL)
+        return true;
+    else 
+        return false;
+}
 void push(){
     noPtr p;
     int valor;
@@ -20,22 +26,23 @@ void push(){
     p->prox = topo;
     topo = p;
 }
-// void pop(){
-//     noPtr p = topo;
-//     if(!listaVazia()){
-//         topo = topo->prox;
-//         delete(p);
-//         cout << "\nO elemento foi retirado da pilha." << endl;
-//     }
-//     else cout << "\nLista vazia!" <<;
-// }
+void pop(){
+    noPtr p = topo;
+    if(!listaVazia()){
+        topo = topo->prox;
+        delete(p); //desalocar memória
+        cout << "\nO elemento foi retirado da pilha." << endl;
+    }
+    else cout << "\nLista vazia!" << endl;
+}
 int main(){
     int op;
     do{
         cin >> op;
         switch(op){
             case 1: push();break;
-            // case 2: pop();break;
+            case 2: pop();break;
+            // case 3: ;break;
         }
     }while(op!=0);
 
